4c904694285928a89db3f77c63b92a792ccc9be6
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-24 18:09:38 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startupjobsbrasil::Application.config.secret_token = 'a5ac8af55bd2cfa018c575758ca3a33c776e6fa489acad755779a65ce8d0ffae91ef813c957bc4de1c787153a3adedea085d1a85d2cde7ad181a2589fa0cec8d'
