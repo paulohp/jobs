f90bc256fa51d851bfbd70875461518d833d2d4f
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-25 15:14:30 -0300

    - footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Startupjobsbrasil::Application.routes.draw do
 
   resources :categories
 
-
+  match "enviar-uma-vaga" => "jobs#new"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
