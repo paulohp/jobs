989c475df1f8ce9c39eeeb327f6b8b5707a6223d
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-25 08:58:29 -0300

    formatando data

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Startupjobsbrasil::Application.initialize!
+Time::DATE_FORMATS[:post] = "posted on %b, %m %Y - %I:%M%p"
