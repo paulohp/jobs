5197f8aae2c1773a163e3ea3b86c6bf6b6b87af4
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-25 12:47:16 -0300

    acts on job

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,4 @@
 class Job < ActiveRecord::Base
+  acts_as_taggable
   attr_accessible :apply_email, :category, :company_description, :company_name, :contact, :description, :keywords, :terms, :title, :website
 end
