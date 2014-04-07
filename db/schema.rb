db0190bf91cd43593bd3708eb8cecdc3086bb16b
Paulo Pires <taralloqueiroiz@gmail.com>
2014-04-07 19:29:37 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010230853) do
+ActiveRecord::Schema.define(:version => 20131010230938) do
 
   create_table "ads", :force => true do |t|
+    t.string   "title"
+    t.text     "desc"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
