7c5c1d78b9e911565c5047f908eaeab2843a157f
Paulo Pires <taralloqueiroiz@gmail.com>
2014-04-09 21:03:50 -0300

    added a active status to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131010230938) do
+ActiveRecord::Schema.define(:version => 20140410000203) do
 
   create_table "ads", :force => true do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20131010230938) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.integer  "category_id"
+    t.boolean  "active"
   end
 
   create_table "taggings", :force => true do |t|
